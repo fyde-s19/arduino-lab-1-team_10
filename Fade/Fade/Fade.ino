c91342ae345071352e2f098392650d1d96133442
JordonKash <jordonkash@utexas.edu>
2019-02-22 16:49:49 -0600

    fade code final
    fade code final

--- Fade/Fade/Fade.ino
+++ Fade/Fade/Fade.ino
@@ -2,6 +2,7 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
+int duty = 90;
 
 void dimmer (int freq, int duty){
   int period, onTime, offTime;
@@ -17,5 +18,12 @@ void dimmer (int freq, int duty){
 
 // the loop function runs over and over again forever
 void loop() {
-  dimmer(10, 90);                       
+  dimmer(100, duty); //100Hz frequency, duty is changing lower and higher
+  
+  if(duty == 90){
+    duty--;  
+  }
+  if(duty == 0){
+    duty++;
+  }                       
 }
